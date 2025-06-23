accb4fc5b2ed380a0629f0a02589ada2dc50c08a
Kota Varun Kumar Reddy <kvkreddy507@gmail.com>
2025-06-23 18:51:29 +0530

    Add 1st program

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello, JADE!");
+        System.out.println("This is my first java program");
+
+    }
+}
